65f995c204733660ebdd1790975ed33b9d1e62b7
Lex <lexluka1031@gmail.com>
2024-12-23 18:37:06 -0600

    Add lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,8 @@
-import { Roboto } from "next/font/google";
+import { Roboto, Lusitana } from "next/font/google";
 
 export const roboto = Roboto({ weight: ["400"], subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
